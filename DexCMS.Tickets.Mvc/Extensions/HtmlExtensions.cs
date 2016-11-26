18b9bc14b066e8f386c20f7f7874333b304bdde5
Cyricx <contact@chrisbyram.com>
2016-11-26 12:11:50 -0600

    Added html extension to mvc project for building open event links

---
+++ DexCMS.Tickets.Mvc/Extensions/HtmlExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DexCMS.Tickets.Mvc.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString BuildEventList(this HtmlHelper html, object events)
+        {
+            if (events != null)
+            {
+                List<Events.Models.Event> evts = (List<Events.Models.Event>)events;
+                TagBuilder ulTag = new TagBuilder("ul");
+                foreach (var evt in evts)
+                {
+                    string segment = evt.EventSeriesID.HasValue ? evt.EventSeries.SeriesUrlSegment : evt.EventUrlSegment;
+
+                    TagBuilder liTag = BuildLiAnchor("~/events/" + segment, evt.PageContent.Heading); ;
+                    ulTag.InnerHtml += liTag.ToString();
+
+                    if (HttpContext.Current.User.IsInRole("Cashier"))
+                    {
+                        ulTag.InnerHtml += BuildLiAnchor("~/secure/checkins/" + segment, evt.PageContent.Heading + " Check In").ToString();
+                    }
+                }
+
+                return new MvcHtmlString(ulTag.ToString());
+            }
+            else
+            {
+                return new MvcHtmlString("");
+            }
+        }
+
+        private static TagBuilder BuildLiAnchor(string url, string text)
+        {
+            TagBuilder liTag = new TagBuilder("li");
+            TagBuilder aTag = new TagBuilder("a");
+            aTag.Attributes["title"] = text;
+
+            aTag.Attributes["href"] = UrlHelper.GenerateContentUrl(url, new HttpContextWrapper(HttpContext.Current));
+
+            aTag.InnerHtml = text;
+
+            liTag.InnerHtml = aTag.ToString();
+            return liTag;
+        }
+    }
+}
---
+++ DexCMS.Tickets.Mvc/Filters/GetPublicOpenEvents.cs
@@ -0,0 +1,25 @@
+﻿using DexCMS.Tickets.Events.Interfaces;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DexCMS.Tickets.Mvc.Filters
+{
+    public class GetPublicOpenEvents : ActionFilterAttribute
+    {
+        IEventRepository repository;
+
+        public GetPublicOpenEvents(IEventRepository _repo)
+        {
+            repository = _repo;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsAjaxRequest() && filterContext.Controller.ViewBag.OpenEvents == null)
+            {
+                filterContext.Controller.ViewBag.OpenEvents = repository.Items.Where(x => x.IsPublic && x.EventEnd > DateTime.Now).OrderBy(x => x.PageContent.Heading).ToList();
+            }
+        }
+    }
+}
