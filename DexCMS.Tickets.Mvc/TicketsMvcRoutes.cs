0f797aaf5916c4035b40bc8803adc938ac22ff57
Cyricx <contact@chrisbyram.com>
2016-11-25 18:37:42 -0600

    Added tickets mvc routes

---
+++ DexCMS.Tickets.Mvc/TicketsMvcRoutes.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Routing;
+using System.Web.Mvc;
+using DexCMS.Core.Infrastructure.Models;
+
+namespace DexCMS.Tickets.Mvc
+{
+    public static class TicketsMvcRoutes
+    {
+        public static void CreateDefaultRoutes(RouteCollection routes, DexCMSConfiguration config)
+        {
+            routes.MapRoute(
+                name: "EvetFaqItems",
+                url: "EventFaqItems/{action}/{id}",
+                defaults: new { controller = "EventFaqItems" });
+
+            routes.MapRoute(
+                name: "Events",
+                url: "Events/{urlSegment}",
+                defaults: new { category = "events", controller = "Events", action = "Index" });
+
+            routes.MapRoute(
+                name: "EventSchedule",
+                url: "Events/{category}/Schedule",
+                defaults: new { urlSegment = "schedule", action = "Schedule", controller = "Events" });
+
+            routes.MapRoute(
+                name: "EventPrice",
+                url: "Events/{category}/Prices",
+                defaults: new { urlSegment = "prices", action = "Prices", controller = "Events" });
+
+            routes.MapRoute(
+                name: "EventRegistration",
+                url: "Events/{category}/Registration",
+                defaults: new { urlSegment = "registration", action = "Registration", controller = "Events" });
+
+            routes.MapRoute(
+                name: "EventFaq",
+                url: "Events/{category}/FAQ",
+                defaults: new { urlSegment = "faq", action = "FAQ", controller = "Events" });
+        }
+    }
+}
