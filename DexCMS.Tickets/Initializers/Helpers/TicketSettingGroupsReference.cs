c91f3d3176a28bb93e44894396769a15cb27b115
Cyricx <contact@chrisbyram.com>
2016-12-11 12:53:50 -0600

    Tickets: Added initializers for settings

---
+++ DexCMS.Tickets/Initializers/Helpers/TicketSettingGroupsReference.cs
@@ -0,0 +1,15 @@
+﻿using DexCMS.Core.Infrastructure.Contexts;
+using DexCMS.Core.Infrastructure.Initializers.Helpers;
+using System.Linq;
+
+namespace DexCMS.Tickets.Initializers.Helpers
+{
+    class TicketSettingGroupsReference:SettingGroupsReference
+    {
+        public int Tickets { get; set; }
+        public TicketSettingGroupsReference(IDexCMSCoreContext context): base(context)
+        {
+            Tickets = context.SettingGroups.Where(x => x.SettingGroupName == "Tickets").Select(x => x.SettingGroupID).Single();
+        }
+    }
+}
---
+++ DexCMS.Tickets/Initializers/SettingGroupInitializer.cs
@@ -0,0 +1,22 @@
+﻿using DexCMS.Core.Infrastructure.Contexts;
+using DexCMS.Core.Infrastructure.Globals;
+using DexCMS.Core.Infrastructure.Models;
+using System.Data.Entity.Migrations;
+
+namespace DexCMS.Tickets.Initializers
+{
+    class SettingGroupInitializer : DexCMSInitializer<IDexCMSCoreContext>
+    {
+        public SettingGroupInitializer(IDexCMSCoreContext context) : base(context)
+        {
+        }
+
+        public override void Run()
+        {
+            Context.SettingGroups.AddOrUpdate(x => x.SettingGroupName,
+                new SettingGroup { SettingGroupName = "Tickets" }
+            );
+            Context.SaveChanges();
+        }
+    }
+}
