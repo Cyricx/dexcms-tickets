7f461fba3ddba23cd0987e1d20c617eef107e6c8
Cyricx <contact@chrisbyram.com>
2016-12-30 13:48:21 -0600

    Updated to use core grunt app file

--- gruntfile.js
+++ gruntfile.js
@@ -1,35 +1,19 @@
 ﻿/// <binding BeforeBuild='clean' AfterBuild='copy' />
 module.exports = function (grunt) {
+
+    var options = {
+        root: 'DexCMS.Tickets'
+    },
+    dexCMSUtilities = require('./node_modules/dexcms-core/DexCMS.Core.Client/utilities');
+
+    var applicationGrunt = dexCMSUtilities.gruntBuilder.application(grunt, options);
+    var gruntOptions = applicationGrunt.builder();
+
     //Configuration setup
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        copy: {
-            domain: {
-                expand: true,
-                cwd: 'DexCMS.Tickets/bin/Release/',
-                src: ['DexCMS.Tickets.dll'],
-                dest: 'dist/'
-            },
-            mvc: {
-                expand: true,
-                cwd: 'DexCMS.Tickets.Mvc/bin/Release/',
-                src: ['DexCMS.Tickets.Mvc.dll'],
-                dest: 'dist/'
-            },
-            webapi: {
-                expand: true,
-                cwd: 'DexCMS.Tickets.WebApi/bin/Release/',
-                src: ['DexCMS.Tickets.WebApi.dll'],
-                dest: 'dist/'
-            }
-        },
-        clean: {
-            build: ["dist"]
-        }
-    });
+    grunt.initConfig(gruntOptions);
+    //load npm tasks
+    applicationGrunt.loadTasks();
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-
-    grunt.registerTask('default', ['clean', 'copy']);
+    //register tasks
+    applicationGrunt.registerTasks();
 };
